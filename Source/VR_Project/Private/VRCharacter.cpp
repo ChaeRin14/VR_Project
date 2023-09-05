9413d1a57cd68f1b672d09645621a3ca67abe0d6
Wonseok Park <amurorei2000@gmail.com>
2023-09-05 11:23:54 +0900

    230905 오전

--- Source/VR_Project/Private/VRCharacter.cpp
+++ Source/VR_Project/Private/VRCharacter.cpp
@@ -2,18 +2,86 @@
 
 
 #include "VRCharacter.h"
+#include "Camera/CameraComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "MotionControllerComponent.h"
+#include "Components/TextRenderComponent.h"
+#include "HeadMountedDisplayFunctionLibrary.h"
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
 
 
 AVRCharacter::AVRCharacter()
 {
 	PrimaryActorTick.bCanEverTick = true;
 
+	hmdCam = CreateDefaultSubobject<UCameraComponent>(TEXT("HMD Camera"));
+	hmdCam->SetupAttachment(RootComponent);
+
+	hmdMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("HMD Mesh"));
+	hmdMesh->SetupAttachment(hmdCam);
+
+	leftMotionController = CreateDefaultSubobject<UMotionControllerComponent>(TEXT("Left Motion Controller"));
+	leftMotionController->SetupAttachment(RootComponent);
+	leftMotionController->SetTrackingMotionSource(FName("Left"));
+
+	leftHand = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("Left Hand Mesh"));
+	leftHand->SetupAttachment(leftMotionController);
+
+	leftLog = CreateDefaultSubobject<UTextRenderComponent>(TEXT("Left Log"));
+	leftLog->SetupAttachment(leftMotionController);
+	leftLog->SetRelativeLocation(FVector(0, 0, 15));
+	leftLog->SetRelativeRotation(FRotator(0, 180, 0));
+	leftLog->SetWorldSize(10);
+	leftLog->SetTextRenderColor(FColor(255, 255, 0, 255));
+	leftLog->SetHorizontalAlignment(EHTA_Center);
+	leftLog->SetVerticalAlignment(EVRTA_TextCenter);
+
+	rightMotionController = CreateDefaultSubobject<UMotionControllerComponent>(TEXT("Right Motion Controller"));
+	rightMotionController->SetupAttachment(RootComponent);
+	rightMotionController->SetTrackingMotionSource(FName("Right"));
+
+	rightHand = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("Right Hand Mesh"));
+	rightHand->SetupAttachment(rightMotionController);
+
+	rightLog = CreateDefaultSubobject<UTextRenderComponent>(TEXT("Right Log"));
+	rightLog->SetupAttachment(rightMotionController);
+	rightLog->SetWorldSize(10);
+	rightLog->SetTextRenderColor(FColor(255, 255, 0, 255));
+	rightLog->SetHorizontalAlignment(EHTA_Center);
+	rightLog->SetVerticalAlignment(EVRTA_TextCenter);
+
+	bUseControllerRotationYaw = true;
+	bUseControllerRotationPitch = true;
+	GetCharacterMovement()->bOrientRotationToMovement = false;
+	GetCharacterMovement()->bUseControllerDesiredRotation = false;
 }
 
 void AVRCharacter::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	// 양손 로그 초기화
+	leftLog->SetText(FText::FromString("Left Log..."));
+	rightLog->SetText(FText::FromString("Right Log..."));
+
+	// 머리 장비 기준점 설정
+	UHeadMountedDisplayFunctionLibrary::SetTrackingOrigin(EHMDTrackingOrigin::Stage);
+
+	pc = GetController<APlayerController>();
+
+	if (pc != nullptr)
+	{
+		UEnhancedInputLocalPlayerSubsystem* subSys = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(pc->GetLocalPlayer());
+
+		if (subSys != nullptr)
+		{
+			subSys->AddMappingContext(imc_VRmap, 0);
+		}
+	}
+
 }
 
 void AVRCharacter::Tick(float DeltaTime)
@@ -26,5 +94,38 @@ void AVRCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCompone
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	UEnhancedInputComponent* enhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent);
+	if (enhancedInputComponent != nullptr)
+	{
+		enhancedInputComponent->BindAction(inputActions[0], ETriggerEvent::Triggered, this, &AVRCharacter::Move);
+		enhancedInputComponent->BindAction(inputActions[0], ETriggerEvent::Completed, this, &AVRCharacter::Move);
+		enhancedInputComponent->BindAction(inputActions[1], ETriggerEvent::Triggered, this, &AVRCharacter::Rotate);
+		enhancedInputComponent->BindAction(inputActions[1], ETriggerEvent::Completed, this, &AVRCharacter::Rotate);
+	}
+}
+
+void AVRCharacter::Move(const FInputActionValue& value)
+{
+	FVector2D controllerInput = value.Get<FVector2D>();
+	leftLog->SetText(FText::FromString(FString::Printf(TEXT("x: %.2f\r\ny: %.2f"), controllerInput.X, controllerInput.Y)));
+
+	FVector forwardVec = FRotationMatrix(GetController()->GetControlRotation()).GetUnitAxis(EAxis::X);
+	FVector rightVec = FRotationMatrix(GetController()->GetControlRotation()).GetUnitAxis(EAxis::Y);
+
+	AddMovementInput(forwardVec, controllerInput.X);
+	AddMovementInput(rightVec, controllerInput.Y);
+}
+
+void AVRCharacter::Rotate(const FInputActionValue& value)
+{
+	FVector2D rightConInput = value.Get<FVector2D>();
+
+	rightLog->SetText(FText::FromString(FString::Printf(TEXT("%.2f\r\n%.2f"), rightConInput.X, rightConInput.Y)));
+
+	if (pc != nullptr)
+	{
+		pc->AddYawInput(rightConInput.X);
+		pc->AddPitchInput(rightConInput.Y);
+	}
 }
 
--- Source/VR_Project/Public/VRCharacter.h
+++ Source/VR_Project/Public/VRCharacter.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "InputActionValue.h"
 #include "VRCharacter.generated.h"
 
 UCLASS()
@@ -22,4 +23,39 @@ public:
 
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+	UPROPERTY(EditAnywhere, Category="MySettings|Components")
+	class UCameraComponent* hmdCam;
+
+	UPROPERTY(EditAnywhere, Category="MySettings|Components")
+	class UStaticMeshComponent* hmdMesh;
+
+	UPROPERTY(EditAnywhere, Category="MySettings|Components")
+	class UMotionControllerComponent* leftMotionController;
+
+	UPROPERTY(EditAnywhere, Category="MySettings|Components")
+	class USkeletalMeshComponent* leftHand;
+
+	UPROPERTY(EditAnywhere, Category="MySettings|Components")
+	class UTextRenderComponent* leftLog;
+
+	UPROPERTY(EditAnywhere, Category="MySettings|Components")
+	class UMotionControllerComponent* rightMotionController;
+
+	UPROPERTY(EditAnywhere, Category="MySettings|Components")
+	class USkeletalMeshComponent* rightHand;
+
+	UPROPERTY(EditAnywhere, Category="MySettings|Components")
+	class UTextRenderComponent* rightLog;
+
+	UPROPERTY(EditAnywhere, Category="MySettings|Inputs")
+	class UInputMappingContext* imc_VRmap;
+
+	UPROPERTY(EditAnywhere, Category="MySettings|Inputs")
+	TArray<class UInputAction*> inputActions;
+
+private:
+	void Move(const FInputActionValue& value);
+	void Rotate(const FInputActionValue& value);
+
+	class APlayerController* pc;
 };
